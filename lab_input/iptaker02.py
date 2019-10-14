0c0c844b16a3346355ec07adf1b78b6fcd70a1a2
Josh Kniepkamp <mr.joshuah@gmail.com>
2019-10-14 11:35:28 -0400

    All Files must go

--- lab_input/iptaker02.py
+++ lab_input/iptaker02.py
@@ -2,4 +2,6 @@
 
 user_input = input("Please enter an IPv4 address: ")
 print ("You told me the IPv4 address is: ", user_input)
+user_input = input("What is your favorite oolor?  ")
+print ("Your favorite color is ", user_input, "..no, yellow!!")
 
